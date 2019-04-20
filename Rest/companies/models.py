ca25625bdd12cdb13cd93422c2ba5d6ca89203b2
Pulkit Jatav <jatavpulkit@gmail.com>
2019-04-20 23:45:06 +0530

    added model

--- Rest/companies/models.py
+++ Rest/companies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Stock(models.Model):
+    ticker = models.CharField(max_length=10)
+    is_open = models.FloatField()
+    is_close = models.FloatField()
+    volume = models.IntegerField()
+    
+    def __str__(self):
+        return self.ticker
